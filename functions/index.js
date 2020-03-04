b6deda616c4c768441161892a11127c56012ca65
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2020-03-04 16:51:59 -0500

    added user authentication

--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,25 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin')
+const firebase = require('firebase')
 const express = require('express');
 const app = express()
+require('dotenv').config()
 
 admin.initializeApp();
 
-
 const config = {
-    apiKey: "AIzaSyAg3a5CzmoEmLNWCq8a1Ak_kGaBV56Hyd4",
-    authDomain: "socialape-c71ad.firebaseapp.com",
-    databaseURL: "https://socialape-c71ad.firebaseio.com",
-    projectId: "socialape-c71ad",
-    storageBucket: "socialape-c71ad.appspot.com",
-    messagingSenderId: "755815002638",
-    appId: "1:755815002638:web:d7203169786ba46a1c425f",
-    measurementId: "G-4JCLSC8ZHM"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,    
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID, 
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 }
 
 
-const firebase = require('firebase')
+
 firebase.initializeApp(config)
 
 app.get('/screams', (req, res) => {
