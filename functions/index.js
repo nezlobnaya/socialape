d1c5ac74992a6fc5c48a7dba0b9867d274870cf9
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2020-03-04 15:35:12 -0500

    user authentification added

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,25 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin')
-
-admin.initializeApp();
-
 const express = require('express');
 const app = express()
 
+admin.initializeApp();
+
+
+const config = {
+    apiKey: "AIzaSyAg3a5CzmoEmLNWCq8a1Ak_kGaBV56Hyd4",
+    authDomain: "socialape-c71ad.firebaseapp.com",
+    databaseURL: "https://socialape-c71ad.firebaseio.com",
+    projectId: "socialape-c71ad",
+    storageBucket: "socialape-c71ad.appspot.com",
+    messagingSenderId: "755815002638",
+    appId: "1:755815002638:web:d7203169786ba46a1c425f",
+    measurementId: "G-4JCLSC8ZHM"
+}
+
+
+const firebase = require('firebase')
+firebase.initializeApp(config)
 
 app.get('/screams', (req, res) => {
     admin
@@ -43,5 +57,24 @@ app.post('/scream', (req, res) => {
             console.error('New scream error',err)
         });
 });
+
+// signup route
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle
+    }
+    //validate data
+ firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then(data => {
+        return res.status(201).json({ message: `user ${data.user.uid} signed up successfully!`})
+    })
+    .catch(err => {
+        console.error(err)
+        return res.status(500).json({ error: err.code })
+    })
+})
 
 exports.api = functions.https.onRequest(app);
